721424cc806886d75313badb89224d370a33aa2e
Manuel de la Peña <mdelapenya@gmail.com>
2024-09-05 17:08:16 +0200

    chore: fix example name

--- elasticsearch-module/main_test.go
+++ elasticsearch-module/main_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/testcontainers/testcontainers-go/modules/elasticsearch"
 )
 
-func ExampleElasticsearch() {
+func ExampleElasticsearchContainer() {
 	ctx := context.Background()
 
 	ctr, err := elasticsearch.Run(ctx, "docker.elastic.co/elasticsearch/elasticsearch:8.14.2")
