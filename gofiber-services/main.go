08b6a5de0c3403a2780a8ca68c7934250ee64b56
Manuel de la Peña <mdelapenya@gmail.com>
2025-05-30 11:01:09 +0200

    Revert "fix: only handle signals while on DEV mode"
    This reverts commit 824650e36ddc520de5ca4d2c2d8e3ef038be95a7.

--- gofiber-services/main.go
+++ gofiber-services/main.go
@@ -48,19 +48,13 @@ func main() {
 		}
 	}()
 
-	// Only handle signals if not running in development mode
-	if os.Getenv("APP_ENV") != "dev" {
-		quit := make(chan os.Signal, 1)                    // Create channel to signify a signal being sent
-		signal.Notify(quit, os.Interrupt, syscall.SIGTERM) // When an interrupt or termination signal is sent, notify the channel
+	quit := make(chan os.Signal, 1)                    // Create channel to signify a signal being sent
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM) // When an interrupt or termination signal is sent, notify the channel
 
-		<-quit // This blocks the main thread until an interrupt is received
-		fmt.Println("Gracefully shutting down...")
-		err = app.Shutdown()
-		if err != nil {
-			log.Panic(err)
-		}
-	} else {
-		// In development mode, just block forever
-		select {}
+	<-quit // This blocks the main thread until an interrupt is received
+	fmt.Println("Gracefully shutting down...")
+	err = app.Shutdown()
+	if err != nil {
+		log.Panic(err)
 	}
 }
